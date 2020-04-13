e701df91165ed995d73c77befdde5e6294a642f2
jiwomdf <mdf.jiwo@gmail.com>
2020-04-13 17:24:29 +0700

    modify footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,17 @@
 <template>
-  <v-footer dark padless>
-    <v-card flat tile class="black lighten-1 white--text text-center flex">
+  <v-footer padless>
+    <v-card flat tile class=" lighten-1 black--text text-center flex">
       <v-card-text>
-       JAVA MASSAGE
       </v-card-text>
 
-      <v-card-text class="white--text">
-        Java Message Jakarta is a official website of Mas Aji Entrepreneurship (Professional Terapis Pijat Jakarta Barat) <br/> Created By Vue Js and Vuetify Library, to see the full code you can open this link <a href="https://github.com/jiwomdf/Vue-Vuetify-Massage-Website">https://github.com/jiwomdf/Vue-Vuetify-Massage-Website</a>.
+      <v-card-text class="black--text">
+        Java Message Jakarta is an official website of Mas Aji Entrepreneurship (Professional Terapis Pijat Jakarta Barat).
       </v-card-text>
 
       <v-divider></v-divider>
 
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} by Katili Jiwo Adi W.
+      <v-card-text class="black--text">
+        {{ new Date().getFullYear() }} © Katili Jiwo Adi W.
       </v-card-text>
     </v-card>
   </v-footer>
