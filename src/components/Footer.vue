f15a32d4cf276a5fcb79b74c55a314a9361e04e4
jiwomdf <mdf.jiwo@gmail.com>
2020-07-12 17:26:07 +0700

    change jakarta -> kediri

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,15 @@
 <template>
   <v-footer padless>
-    <v-card flat tile class=" lighten-1 black--text text-center flex">
-      <v-card-text>
-      </v-card-text>
+    <v-card flat tile class="lighten-1 black--text text-center flex">
+      <v-card-text></v-card-text>
 
-      <v-card-text class="black--text">
-        Java Message Jakarta is an official website of Mas Aji Entrepreneurship (Professional Terapis Pijat Jakarta Barat).
-      </v-card-text>
+      <v-card-text
+        class="black--text"
+      >Java Message Kediri is an official website of Mas Aji Entrepreneurship (Professional Terapis Pijat Kediri).</v-card-text>
 
       <v-divider></v-divider>
 
-      <v-card-text class="black--text">
-        {{ new Date().getFullYear() }} © Katili Jiwo Adi W.
-      </v-card-text>
+      <v-card-text class="black--text">{{ new Date().getFullYear() }} © Katili Jiwo Adi W.</v-card-text>
     </v-card>
   </v-footer>
 </template>
